adfb9029c28b6e1bbf07b60dc1941a6043c5e6a9
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-08-20 20:26:27 +0700

    add: product image

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -7,14 +7,16 @@
 
     <div class="grid gap-5 grid-cols-5">
         @foreach ($products as $product)
-        <div class="bg-white border rounded-lg shadow-lg">
-            <div class="w-full h-52 bg-gray-200 rounded-t-lg" style="background-image: url({{$product->image}}); background-size: cover; background-position: center;"></div>
+        <button>
+            <div class="bg-white border rounded-lg shadow-lg text-left">
+                <div class="w-full h-52 bg-gray-200 rounded-t-lg" style="background-image: url({{$product->image}}); background-size: cover; background-position: center;"></div>
 
-            <div class="p-3">
-                <p class="text-ellipsis overflow-hidden whitespace-nowrap mb-2">{{$product->name}}</p>
-                <p class="font-bold text-sm">Rp {{number_format($product->price,0,",", ".")}}</p>
+                <div class="p-3">
+                    <p class="text-ellipsis overflow-hidden whitespace-nowrap mb-2">{{$product->name}}</p>
+                    <p class="font-bold text-sm">Rp {{number_format($product->price,0,",", ".")}}</p>
+                </div>
             </div>
-        </div>
+        </button>
         @endforeach
     </div>
 
