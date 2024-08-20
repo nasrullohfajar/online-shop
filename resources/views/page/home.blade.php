482997a3868acc09b53555fa982ed4f2aec7a6ee
nasrullohfajarm <nasrullohfajar10@gmail.com>
2024-08-20 14:54:30 +0700

    add: product list

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -2,8 +2,18 @@
 
 @section('content')
 
-<div class="flex flex-col py-5 gap-y-4 max-w-[1200px] mx-auto">
+<div class="flex flex-col py-5 gap-y-5 max-w-[1200px] mx-auto">
     <div class="bg-hero rounded-xl min-h-[420px] min-w-full"></div>
+
+    <div class="grid gap-5 grid-cols-5">
+        <div class="w-56 bg-white border rounded-lg shadow-lg">
+            <div class="w-full h-52 bg-blue-900 rounded-t-lg"></div>
+            <div class="p-3">
+                <p class="text-ellipsis overflow-hidden whitespace-nowrap mb-2">Title</p>
+                <p class="font-bold text-sm mb-2">Price</p>
+            </div>   
+        </div>
+    </div>
 </div>
 
 
