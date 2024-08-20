882cc19a83f0c2cf9e4e55138c188575dc23cd64
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-08-20 09:22:19 +0700

    add: navbar

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Online Shop</title>
+</head>
+
+<body>
+    <Header>
+        <Nav class="flex items-center justify-between h-16 px-20 text-[#1a1a1a] border border-b-1 shadow-sm">
+            <h1 class=" text-xl">Online Shop</h1>
+        </Nav>
+    </Header>
+
+    <Main></Main>
+
+    <Footer></Footer>
+</body>
+
+</html>
