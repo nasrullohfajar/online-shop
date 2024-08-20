3754f7b0792c5d1a5cf448aed6f09b138ebc562e
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-08-20 18:20:20 +0700

    fix: format product price

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -8,11 +8,11 @@
     <div class="grid gap-5 grid-cols-5">
         @foreach ($products as $product)
         <div class="bg-white border rounded-lg shadow-lg">
-            <div class="w-full h-52 bg-gray-400 rounded-t-lg" style="background-image: url({{$product->image}}); background-size: cover; background-position: center;"></div>
+            <div class="w-full h-52 bg-gray-200 rounded-t-lg" style="background-image: url({{$product->image}}); background-size: cover; background-position: center;"></div>
 
             <div class="p-3">
                 <p class="text-ellipsis overflow-hidden whitespace-nowrap mb-2">{{$product->name}}</p>
-                <p class="font-bold text-sm">Rp {{$product->price}}</p>
+                <p class="font-bold text-sm">Rp {{number_format($product->price,0,",", ".")}}</p>
             </div>
         </div>
         @endforeach
